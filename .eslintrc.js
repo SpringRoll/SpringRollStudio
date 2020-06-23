069e2ca30cc688736d8368d1d75b8469d1a07568
amalley <aaron.malley@redspace.com>
2020-06-23 16:46:18 -0300

    Removed generator star spacing eslinting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,8 +39,6 @@ module.exports = {
 
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
