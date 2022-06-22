8dfdd84708b968b1027aa74b575ea158f3841815
Rob Carter <rtcarter@pbs.org>
2022-06-22 15:10:48 -0400

    electron bump, removed overrides, runs at node 14+

--- src/main/index.js
+++ src/main/index.js
@@ -44,6 +44,7 @@ function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      contextIsolation: false,
       webviewTag: true,
       webSecurity: false
     }
